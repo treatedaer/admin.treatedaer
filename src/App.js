14ff15bf617a1a31f4403a3e41411435b9f79170
treatedaer <clientnatvi@gmail.com>
2022-12-02 00:00:32 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -29,41 +29,65 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 var ddatabase_table = [];
 var feedbacktable_table = [];
-
+var BookAMeeting_table = [];
 const Rightside =()=> {
   const [updated, refresh] = useState(0);
   const [status, setstatus] = useState({});
+  const [status_meetingform, setstatus_meetingform] = useState({});
   const [openmenubar, setOpenmenubar] = useState(0);
-  const refArray = useRef([]);
+  const qutotenotationarr = useRef([]);
+  const BookAMeetingarr = useRef([]);
   const userid = useRef(null);
   const psd = useRef(null);
-  const updatestatus = index => {
+  const updatestatus = (index) => {
   
-    var id = document.getElementsByClassName("datainput")[index].getAttribute("data-label");
-    var value =document.getElementsByClassName("datainput")[index].innerHTML;
+ 
+    var value =document.getElementsByClassName("datainput "+index)[0].innerHTML;
+    alert(value)
     const updates={};
-    updates['/Technical/' + id+'/status'] = value;
+    updates['/Quotedeatils/' + index+'/status'] = value;
     ddatabase_table=[]
     update(ref(db), updates)
 
     ddatabase_table=[]
     
   }
+  const updatestatus_bookmeeting = (index) => {
+  
+ 
+    var value =document.getElementsByClassName("datainput_meeting "+index)[0].innerHTML;
+    alert(value)
+    const updates={};
+    updates['/Meeting/' + index+'/status'] = value;
+    ddatabase_table=[]
+    update(ref(db), updates)
 
+    ddatabase_table=[]
+    
+  }
   const handleChange = (event) => {
      event.target.value=0;
     setstatus({...status,key1:{value:event.target.getAttribute("data-label")}})
+   
   
    };  
+   const handleChange_meetingform = (event) => {
+    event.target.value=0;
+    setstatus_meetingform({...status,key1:{value:event.target.getAttribute("data-label")}})
+  
+ 
+  }; 
   const get_details=()=>{
     if(ddatabase_table.length==0){ 
       onValue(starCountRef, (snapshot) => {
       ddatabase_table=[]
-      if(snapshot.val().hasOwnProperty("Technical")){
+      if(snapshot.val().hasOwnProperty("Quotedeatils")){
+   
         let i=-1
-        for (var key in snapshot.val().Technical) {
+        console.log(snapshot.val())
+        for (var key in snapshot.val().Quotedeatils) {
           i=i+1
-          console.log(snapshot.val().Technical[key])
+        
         
           ddatabase_table.push(
   <li>
@@ -72,21 +96,21 @@ const Rightside =()=> {
     <div className="card__header">
 
       <div className="card__header-text">
-        <h3 className="card__title">{snapshot.val().Technical[key]["name"]}<div className="emailid">{snapshot.val().Technical[key]["email"]}</div></h3>    
-        <div className="phonenumber"><i class="fas fa-clock"></i> {snapshot.val().Technical[key]["date"].split("|")[0]}</div>
-        <div className="phonenumber"><i class="fas fa-calendar-alt"></i> {snapshot.val().Technical[key]["date"].split("|")[1]}</div>
-        <div className='phonenumber'><i class="fa fa-phone"></i> {snapshot.val().Technical[key]["phone"]}</div>
-        <div className='phonenumber'><i class="fa fa-twitter"></i> {snapshot.val().Technical[key]["status"]}</div>
+        <h3 className="card__title">{snapshot.val().Quotedeatils[key]["name"]}<div className="emailid">{snapshot.val().Quotedeatils[key]["email"]}</div></h3>    
+        <div className="phonenumber"><i class="fas fa-clock"></i> {snapshot.val().Quotedeatils[key]["date"].split("|")[0]}</div>
+        <div className="phonenumber"><i class="fas fa-calendar-alt"></i> {snapshot.val().Quotedeatils[key]["date"].split("|")[1]}</div>
+        <div className='phonenumber'><i class="fa fa-phone"></i> {snapshot.val().Quotedeatils[key]["phone"]}</div>
+        <div className='phonenumber'><i class="fa fa-twitter"></i> {snapshot.val().Quotedeatils[key]["status"]}</div>
       </div>
     </div>
-    <p className="card__description">{snapshot.val().Technical[key]["quote"]} </p>
+    <p className="card__description">{snapshot.val().Quotedeatils[key]["quote"]} </p>
     <div onChange={handleChange}      ref={ref => {
-            refArray.current[i] = ref; // took this from your guide's example.
-          }} contenteditable="true" data-label={snapshot.val().Technical[key]["cusid"]} className="datainput">
-    {snapshot.val().Technical[key]["status"]}
+            qutotenotationarr.current[i] = ref; 
+          }} contenteditable="true" data-label={snapshot.val().Quotedeatils[key]["cusid"]} className={"datainput "+snapshot.val().Quotedeatils[key]["cusid"]}>
+    {snapshot.val().Quotedeatils[key]["status"]}
 </div>
 
-    <button  onClick={updatestatus.bind(null,i)} data-label={snapshot.val().Technical[key]["cusid"]} >Update</button>
+    <button  onClick={(evt)=>updatestatus(evt.target.getAttribute("data-label"))} data-label={snapshot.val().Quotedeatils[key]["cusid"]} >Update</button>
   </div>
 
 </li>
@@ -107,7 +131,7 @@ const Rightside =()=> {
     
           <div className="card__header-text">
             <h3 className="card__title">{snapshot.val().ContactUs[key]["name"]}<div className="emailid">{snapshot.val().ContactUs[key]["email"]}</div></h3>    
- 
+            <div className='phonenumber'><i class="fa fa-phone"></i> {snapshot.val().ContactUs[key]["phone"]}</div>
             <div className='phonenumber'><i class="fa fa-phone"></i> {snapshot.val().ContactUs[key]["phone"]}</div>
             <div className='phonenumber'><i class="fa fa-twitter"></i> {snapshot.val().ContactUs[key]["Message"]}</div>
           </div>
@@ -118,7 +142,48 @@ const Rightside =()=> {
     
     </li>)
   }
-}  
+}
+if(snapshot.val().hasOwnProperty("Meeting")){
+  BookAMeeting_table=[]
+  let i=0
+  for (var key in snapshot.val().Meeting) {
+    let i=+1
+    BookAMeeting_table.push(  <li>
+  
+      <div className="card__overlay">
+        <div className="card__header">
+    
+          <div className="card__header-text">
+            <h3 className="card__title">{snapshot.val().Meeting[key]["name"]}<div className="emailid">{snapshot.val().Meeting[key]["email"]}</div></h3>    
+ 
+            <div className='phonenumber'><i class="fa fa-phone"></i> {snapshot.val().Meeting[key]["phone"]}</div>
+            <div className='phonenumber'> DateTime of Submission: {snapshot.val().Meeting[key]["quote"]}</div>
+            <div className='phonenumber'> Meeting Time Date: {snapshot.val().Meeting[key]["meeting_datetime"]}</div>
+            <div className='phonenumber'> Status: {snapshot.val().Meeting[key]["status"]}</div>
+          </div>
+        </div>
+        <p className="card__description">{snapshot.val().Meeting[key]["quote"]} </p>
+        <div onChange={handleChange}      ref={ref => {
+            BookAMeetingarr.current[i] = ref; 
+          }} contenteditable="true" data-label={snapshot.val().Meeting[key]["cusid"]}className={"datainput_meeting "+snapshot.val().Meeting[key]["cusid"]}>
+    {snapshot.val().Meeting[key]["status"]}
+</div>
+
+<button  onClick={(evt)=>updatestatus_bookmeeting(evt.target.getAttribute("data-label"))} data-label={snapshot.val().Meeting[key]["cusid"]} >Update</button>
+        
+      </div>
+    
+    </li>)
+  }
+}
+
+
+
+
+
+
+
+
     refresh(1)
     document.getElementsByClassName("tablinks")[0].click()
     });
@@ -210,7 +275,7 @@ return (
 
 <div id="BookAMeeting" className="tabcontent">
   <h3>Book a meeting Form Inputs</h3>
-
+  {updated? BookAMeeting_table:null}   
 </div>
 
 <div id="Cotactus" className="tabcontent">
